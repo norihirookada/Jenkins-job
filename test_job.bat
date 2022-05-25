614f8d2017fb89c73344f6a9e5b21f812bb35209
norihirookada <norihiro-okada@mamezou2.com>
2022-05-25 17:17:56 +0900

    adopt Nishio's cicd_sample2 again

--- test_job.bat
+++ test_job.bat
@@ -1,11 +1,10 @@
-echo "Hello Jenkins !"
+echo "Hello, Jenkins!"
+echo %date% %time%
 
 cd docker_mysql
 
-winpty
+rem winpty
 
 docker-compose up -d
-
 docker-compose ps
-
 docker exec docker_mysql_db_1 bash -c "cd /var && ls -al"
