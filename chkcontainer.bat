69e67a14eb2191a7a6e7e3735c91b281379d8ed7
norihirookada <norihiro-okada@mamezou2.com>
2022-05-26 14:14:38 +0900

    chkcontainer.bat created

---
+++ chkcontainer.bat
@@ -0,0 +1,18 @@
+echo "Hello, Jenkins!"
+echo %date% %time%
+
+cd docker_mysql
+
+rem winpty
+
+docker-compose up -d
+docker-compose ps
+
+docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./failure.sh"
+docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./success.sh"
+IF %ERRORLEVEL% == 0 (
+   docker stop docker_mysql_db_1
+   docker rm docker_mysql_db_1
+) ELSE (
+    echo Error at %time%, %date%
+)
