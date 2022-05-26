89cdbdd36187229177c304f719c55beb4c5ca2a9
norihirookada <norihiro-okada@mamezou2.com>
2022-05-26 18:18:00 +0900

    add "docker rmi"

--- chkcontainer.bat
+++ chkcontainer.bat
@@ -9,10 +9,18 @@ docker-compose up -d
 docker-compose ps
 
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./failure.sh"
+IF %ERRORLEVEL% == 0 (
+   docker stop docker_mysql_db_1
+   docker rm docker_mysql_db_1
+   docker rmi docker_mysql_db_1
+) ELSE (
+    echo Error at %time%, %date%
+)
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./success.sh"
 IF %ERRORLEVEL% == 0 (
    docker stop docker_mysql_db_1
    docker rm docker_mysql_db_1
+   docker rmi docker_mysql_db_1
 ) ELSE (
     echo Error at %time%, %date%
 )
