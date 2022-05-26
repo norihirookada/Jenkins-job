c3c7f16106c10a1b30bd83f2d03b0f05a78902bf
norihirookada <norihiro-okada@mamezou2.com>
2022-05-26 09:00:26 +0900

    run failure.sh, then success.sh

--- test_job.bat
+++ test_job.bat
@@ -7,5 +7,5 @@ rem winpty
 
 docker-compose up -d
 docker-compose ps
+docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./failure.sh"
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./success.sh"
-rem docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./failure.sh"
