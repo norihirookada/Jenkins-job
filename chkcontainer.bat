5109be385cf48dc7d9cd0dfc2154fa70412e718c
norihirookada <norihiro-okada@mamezou2.com>
2022-05-27 11:54:18 +0900

    add comments

--- chkcontainer.bat
+++ chkcontainer.bat
@@ -4,26 +4,41 @@ echo %date% %time%
 
 cd docker_mysql
 
-rem winpty
-
+echo '----------'
+echo 'docker-compose up -d'
 docker-compose up -d
+
+echo '----------'
+echo 'docker-compose ps'
 docker-compose ps
+
+echo '----------'
+echo 'docker ps -a'
 docker ps -a
 
+echo '----------'
+echo 'docker images -a'
+docker images -a
+
+echo '------------------------------------------------------------'
+echo 'failure'
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./failure.sh"
 IF %ERRORLEVEL% == 0 (
     echo Delete docker_mysql_db_1
-    docker stop docker_mysql_db_1
-    docker rm docker_mysql_db_1
-    docker rmi docker_mysql_db_1
+    rem docker stop docker_mysql_db_1
+    rem docker rm docker_mysql_db_1
+    rem docker rmi docker_mysql_db_1
 ) ELSE (
     echo failure.sh resulted in faiure
 )
+
+echo '------------------------------------------------------------'
+echo 'success'
 docker exec docker_mysql_db_1 bash -c "cd /var/dummy && ./success.sh"
 IF %ERRORLEVEL% == 0 (
     docker stop docker_mysql_db_1
     docker rm docker_mysql_db_1
-    docker rmi docker_mysql_db_1
+    echo '"docker rmi ..." for yourself'
 ) ELSE (
     echo success.sh resulted in faiure
 )
